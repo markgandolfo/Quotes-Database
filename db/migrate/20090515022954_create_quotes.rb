4d8fb2fd0b1f82c99488e608b74dafef14454392
Mark Gandolfo <mark@aussiev8.com.au>
2009-05-15 12:31:57 +1000

    added quotes, and users

---
+++ db/migrate/20090515022954_create_quotes.rb
@@ -0,0 +1,16 @@
+class CreateQuotes < ActiveRecord::Migration
+  def self.up
+    create_table :quotes do |t|
+      t.string :from
+      t.string :to
+      t.text :context
+      t.text :quote
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :quotes
+  end
+end
